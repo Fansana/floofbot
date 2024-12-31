f74bda58a6bac895a2ec29d788344eff4c0fab51
Tim <timfalken@hotmail.com>
2024-12-31 18:02:58 +0100

    shorter

--- buttons.js
+++ buttons.js
@@ -128,7 +128,7 @@ module.exports = [
 
             const reasonInput = new TextInputBuilder()
                 .setCustomId('reasonInput')
-                .setLabel(changedVote ? "Note that your vote has already been changed to \"Yes\"! Make sure to update your reason if necessary." : "Your vote requires reasoning to be valid.")
+                .setLabel(changedVote ? "Vote changed to \"Yes\"! Update your reason if needed." : "Your vote requires reasoning to be valid.")
                 .setStyle(TextInputStyle.Paragraph)
                 .setPlaceholder('What is the reason for voting "Yes"?')
                 .setRequired(true)
@@ -181,7 +181,7 @@ module.exports = [
 
             const reasonInput = new TextInputBuilder()
                 .setCustomId('reasonInput')
-                .setLabel(changedVote ? "Note that your vote has already been changed to \"No\"! Make sure to update your reason if necessary." : "Your vote requires reasoning to be valid.")
+                .setLabel(changedVote ? "Vote changed to \"No\"! Update your reason if needed." : "Your vote requires reasoning to be valid.")
                 .setStyle(TextInputStyle.Paragraph)
                 .setPlaceholder('What is the reason for voting "No"?')
                 .setRequired(true)
